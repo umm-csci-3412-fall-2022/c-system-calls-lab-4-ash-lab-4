40d8017b03d4de6358edc01ed5c81dddcd0f4453
Ash <plase024@morris.umn.edu>
2022-10-06 15:10:45 -0500

    Summarize Tree: Finish ftw

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -5,57 +5,21 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <ftw.h>
 
 static int num_dirs, num_regular;
 
-bool is_dir(const char* path) {
-    /*
-     * Use the stat() function (try "man 2 stat") to determine if the file
-     * referenced by path is a directory or not. Call stat, and then use
-     * S_ISDIR to see if the file is a directory. Make sure you check the
-     * return value from stat() in case there is a problem, e.g., maybe the
-     * the file doesn't actually exist.
-     *
-     * You'll need to allocate memory for a buffer you pass to stat(); make
-     * sure you free up that space before you return from this function or
-     * you'll have a substantial memory leak (think of how many times this
-     * will be called!).
-     */
-}
+#define MAX_FTW_DEPTH 16
 
-/*
- * This is necessary this because the mutual recursion means there's no way to
- * order them so that all the functions are defined before they're called.
- */
-void process_path(const char*);
-
-void process_directory(const char* path) {
-    /*
-     * Update the number of directories seen, use opendir() to open the
-     * directory, and then use readdir() to loop through the entries
-     * and process them. You have to be careful not to process the
-     * "." and ".." directory entries, or you'll end up spinning in
-     * (infinite) loops. Also make sure you closedir() when you're done.
-     *
-     * You'll also want to use chdir() to move into this new directory,
-     * with a matching call to chdir() to move back out of it when you're
-     * done.
-     */
-}
-
-void process_file(const char* path) {
-    /*
-     * Update the number of regular files.
-     * This is as simple as it seems. :-)
-     */
-}
-
-void process_path(const char* path) {
-    if (is_dir(path)) {
-        process_directory(path);
-    } else {
-        process_file(path);
+static int callback(const char* fpath, const struct stat *sb, int typeflag) {
+    if (S_ISDIR(sb->st_mode)) {
+        num_dirs++;
     }
+    else {
+        num_regular++;
+    }
+
+    return 0;
 }
 
 int main (int argc, char *argv[]) {
@@ -69,7 +33,7 @@ int main (int argc, char *argv[]) {
     num_dirs = 0;
     num_regular = 0;
 
-    process_path(argv[1]);
+    ftw(argv[1], callback, MAX_FTW_DEPTH);
 
     printf("There were %d directories.\n", num_dirs);
     printf("There were %d regular files.\n", num_regular);
